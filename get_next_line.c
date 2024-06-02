fd95471df25d2afe59e9b4969b8e79d0a57bd354
Weerasinghe Arachchige Kavini Wageesha Weerasinghe <wweerasi@c1r5p1.hive.fi>
2024-06-02 20:26:31 +0300

    Fixed few errors. Still hopeless

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 17:05:58 by wweerasi          #+#    #+#             */
-/*   Updated: 2024/05/31 16:42:34 by wweerasi         ###   ########.fr       */
+/*   Updated: 2024/06/02 20:25:08 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,11 @@ static char *l_construct(char **stash)
 
 	if (**stash)
 	{
-		line = ft_substr(*stash, 0, (ft_strchr(*stash,'\n') - *stash  + 2));
+		line = ft_substr(*stash, 0, (ft_strchr(*stash,'\n') - *stash  + 1));
 		if (line)
         {
             res = ft_strdup(ft_strchr(*stash, '\n') + 1);
+			ft_free(*stash);
             *stash = ft_strdup(res);
             ft_free(res);
         }
@@ -40,7 +41,7 @@ static char *l_construct(char **stash)
             ft_free(*stash);
 	}
 	else
-		line = ft_strdup("");
+		line  = ft_strdup("");
 	return (line);
 }
 
@@ -48,18 +49,20 @@ static char *l_read(int fd, char *buf, char **stash)
 {
     int     bytes_read;
     char    *tmp_stash;
-    char    *line;
+    char    *line; 
 
 	line = NULL;
     bytes_read = BUFFER_SIZE;
-    while(!ft_strchr(*stash, '\n'))
+	if (!*stash || eof)
+		return (NULL);
+    while(!ft_strchr(*stash, '\n'))//bytes_read are useless here because it updates.
     {
         bytes_read = read(fd, buf, BUFFER_SIZE);
-        if (bytes_read == -1)
+		printf("bytes_read: %i\n", bytes_read);
+		if (bytes_read == -1)
             return (ft_free(*stash));
         if (bytes_read == 0)
-			return (NULL);
-            //break;
+			return (ft_free(*stash));
         buf[bytes_read] = '\0';
 		tmp_stash = *stash;
         *stash = ft_strjoin(tmp_stash, buf);
@@ -67,6 +70,7 @@ static char *l_read(int fd, char *buf, char **stash)
 		if (!stash)
 			return(ft_free(*stash));
 	}
+	printf("stash: %s\n", *stash);
 	line = l_construct(stash);
 	return(line);
 }
@@ -82,10 +86,14 @@ char *get_next_line(int fd)
     if (!stash)
         stash = ft_strdup("");
 	if (!stash)
-		return (ft_free(stash));
+		return (NULL);
     line = l_read(fd, buffer, &stash);
     if (!line)
+	{
         line = ft_free(line);
+		if (!stash)
+			return(ft_free(stash));
+	}
     return (line);
 }
 
@@ -113,22 +121,28 @@ int main(void)
 	bytes_read = read(fd, buf, BUFFER_SIZE);
 	printf("Buffer: -%s-, Bytes_read: %i\n", buf, bytes_read);
 }*/
-/*
+
 //main to test GNL function
 int main()
 {
 	int fd;
 	char *line;
 	static int i;
-	
+	char *test = "1234567k9abcdef";
+	int len;
+
+	printf("test len: %lu\n", strlen(test));
+	len = ft_strchr(test, 'k') - test;
+	printf("strchr_len: %i\n", len);
 	line = ft_strdup("");
 	fd = open("ISF.txt", O_RDONLY);
-	while (line)
+	while (i < 30)
 	{
 		line = get_next_line(fd);
 		printf("line [%i] : %s", i++, line);
+		free(line);
 	}
-}*/
+}
 /*
 //main to test if  line extraction logic works
 int main(void)
@@ -159,4 +173,9 @@ int main(void)
 	}
 }*/
 
+
+
+//Theres a error when EOF is reached but called the function again.
+//This ->: a.out(9610,0x117b515c0) malloc: *** error for object 0x7ff65d402a10: pointer being freed was not allocated
+//This code does not consider the matter when there is no new line but a null terminator ending. and we cannot add a check to while loop for null terminator because the stash itself is initiated with a null terminator.
 
